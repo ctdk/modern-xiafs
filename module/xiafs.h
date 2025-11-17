b29a9b9d5ca494df391f503bf2fe50f03e79c782
ctdk <jbingham@gmail.com>
2025-11-17 15:17:22 -0800

    finish up xiafs.h

--- module/xiafs.h
+++ module/xiafs.h
@@ -141,24 +141,65 @@ struct xiafs_inode {		/* 64 bytes */
  * linux super-block data on disk
  */
 struct xiafs_super_block {
+
+// Historically, a bootloader would have gone here. Trying to use xiafs for a
+// bootable filesystem now would be prettyn nuts at best, but the first 512
+// bytes of the block are still reserved. These are all set by `mkxfs` on
+// filesystem creation.
+
     u_char  s_boot_segment[512];	/*  1st sector reserved for boot */
+
+// Always 1024.
+
     __u32  s_zone_size;		/*  0: the name says it		 */
+
+// Accounting for the number of blocks (or zones, as xiafs generally refers to
+// them), the number of inodes, the location of the first data zone, and maps
+// of inodes and blocks.
+
     __u32  s_nzones;			/*  1: volume size, zone aligned */ 
     __u32  s_ninodes;			/*  2: # of inodes		 */
     __u32  s_ndatazones;		/*  3: # of data zones		 */
     __u32  s_imap_zones;		/*  4: # of imap zones           */
     __u32  s_zmap_zones;		/*  5: # of zmap zones		 */
     __u32  s_firstdatazone;		/*  6: first data zone           */
+
+// Always ends up being 0, since the block size is always 1024 bytes.
+
     __u32  s_zone_shift;		/*  7: z size = 1KB << z shift   */
+
+// How big a file can get. As mentioned earlier, because of xiafs' hard-coded
+// 1024 byte blocks, this limit is roughly 64MB.
+
     __u32  s_max_size;			/*  8: max size of a single file */
     __u32  s_reserved0;		/*  9: reserved			 */
     __u32  s_reserved1;		/* 10: 				 */
     __u32  s_reserved2;		/* 11:				 */
     __u32  s_reserved3;		/* 12:				 */
+
+// These are, again, only of historic interest.
     __u32  s_firstkernzone;		/* 13: first kernel zone	 */
     __u32  s_kernzones;		/* 14: kernel size in zones	 */
+
+// Holds the magic number used to identify this as a xiafs filesystem.
+
     __u32  s_magic;			/* 15: magic number for xiafs    */
 };
+
+// # The Directory Entry
+//
+// Since inodes hold no informatin about the name of the file, that information
+// is stored in a directory entry instead. This is how you can have multiple
+// names referring to one file with hard links and why you can't hard link files
+// across different filesystems. At the same time, the directory entry saves no
+// information on the file's ownership, permissions, etc; that information is
+// all stored in the inode.
+//
+// It all fits into 256 bytes, which is why `_XIAFS_NAME_LEN` has the somewhat
+// unintuitive value of 248 bytes. Unlike the Minix fs, but like ext2, xiafs has
+// variable length directory entries. The directory entry struct holds the
+// inode, length of the directory entry rectord, the file name length, and the
+// file name. See `dir.c` for more on this.
 
 struct xiafs_direct {
     __u32   d_ino;
@@ -177,6 +218,12 @@ struct xiafs_direct {
  * Copyright (C) Linus Torvalds, 1991, 1992.
  */
 
+// # Inode Info
+//
+// Holds information about the xiafs inode. Simpler than the equivalanet in
+// ext2, `xiafs_inode_info` only has block pointers and a pointer to a
+// `vfs_inode` struct.
+
 struct xiafs_inode_info {               /* for data zone pointers */
     __u32  i_zone[_XIAFS_NUM_BLOCK_POINTERS];
     struct inode vfs_inode;
@@ -192,8 +239,16 @@ struct xiafs_inode_info {               /* for data zone pointers */
  * Copyright (C) Linus Torvalds, 1991, 1992.
  */
 
+// The number of inode and zone (block) bitmap slots.
+
 #define _XIAFS_IMAP_SLOTS 8
 #define _XIAFS_ZMAP_SLOTS 32
+
+// # Superblock Info
+//
+// Used by the kernel to hold the filesystem's superblock metadata in memory
+// while the filesystem is mounted. The fields here have the same meaning that
+// they do above.
 
 struct xiafs_sb_info {
     u_long   s_nzones;
@@ -221,6 +276,12 @@ extern char internal_error_message[];
 #define INTERN_ERR              internal_error_message, __FILE__, __LINE__
 #define WHERE_ERR               __FILE__, __LINE__
 
+// These macros help simplify some calculations to determine block size, the
+// number of addresses and inodes per block, the number of bits per block, and
+// the value of some bitshifts for counting free blocks and zones in `bitmap.c`.
+// Of course, since the block size is hard coded these values never actually
+// change.
+
 #define XIAFS_ZSHIFT(sp)                ((sp)->s_zone_shift)
 #define XIAFS_ZSIZE(sp)         (BLOCK_SIZE << XIAFS_ZSHIFT(sp))
 #define XIAFS_ZSIZE_BITS(sp)    (BLOCK_SIZE_BITS + XIAFS_ZSHIFT(sp))
@@ -229,6 +290,14 @@ extern char internal_error_message[];
 #define XIAFS_BITS_PER_Z(sp)    (BLOCK_SIZE  << (3 + XIAFS_ZSHIFT(sp)))
 #define XIAFS_BITS_PER_Z_BITS(sp)       (BLOCK_SIZE_BITS + 3 + XIAFS_ZSHIFT(sp))
 #define XIAFS_INODES_PER_Z(sp)  (_XIAFS_INODES_PER_BLOCK << XIAFS_ZSHIFT(sp))
+
+// This part's frankly pretty nuts. Long ago, in order to free up some space in
+// the someone cramped 64 byte xiafs inode, the value that would normally be
+// kept in `i_blocks` in most filesystem inodes (namely, the number of blocks
+// the inode is using) got moved out of its own field into the topmost byte of
+// the first three block pointers. These macros extract that information and put
+// it back, respectively. If nothing else, this reminds us that sometimes space
+// and memory are precious.
 
 /* Use the most significant bytes of zone pointers to store block counter. */
 /* This is ugly, but it works. Note, We have another 7 bytes for "expansion". */
@@ -246,15 +315,25 @@ extern char internal_error_message[];
 
 /* sb_info taken from 2.6.32 minix */
 
+// Unsurprisingly, returns the superblock information.
+
 static inline struct xiafs_sb_info *xiafs_sb(struct super_block *sb)
 {
         return sb->s_fs_info;
 }
 
+// Much like the previous function, this helper function returns the inode's
+// `xiafs_inode_info`.
+
 static inline struct xiafs_inode_info *xiafs_i(struct inode *inode)
 {
         return list_entry(inode, struct xiafs_inode_info, vfs_inode);
 }
+
+// A macro and function that are only useful for hacking on the filesystem. That
+// said, this proved to be very useful when trying to discern what on earth was
+// going on and why something wouldn't work. Once you do figure out what was
+// wrong, though, calls to this should be removed. Your logs will thank you.
 
 /* be able to dump out data */
 #define PRINT_OPAQUE_DATA(p)  print_mem((p), sizeof(*(p)))
@@ -267,6 +346,10 @@ static inline void print_mem(void const *vp, size_t n){
 	}
 
 #ifdef __KERNEL__
+
+// Function definitions. The meat of these is kept in their respective .c files.
+// **N.B.** These headers have a habit of changing subtly between kernel
+// versions because of updates to filesystem APIs elsewhere in the kernel. 
 
 extern struct inode * xiafs_new_inode(const struct inode * dir, umode_t mode, int * error);
 extern void xiafs_free_inode(struct inode * inode);
