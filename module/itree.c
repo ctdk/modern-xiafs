f58e5f16d4be71860d756563397ba0bbfd6a6054
ctdk <jbingham@gmail.com>
2025-11-19 14:10:34 -0800

    finished itree.c

--- module/itree.c
+++ module/itree.c
@@ -402,34 +402,84 @@ static inline int get_block(struct inode * inode, sector_t block,
 			struct buffer_head *bh, int create)
 {
 	int err = -EIO;
+
+// Stores the values used to get the block we want. See `block_to_path` for how
+// `offsets` is filled in.
+
 	int offsets[DEPTH];
+
+// The block chain itself.
+
 	Indirect chain[DEPTH];
+
+// If there's only a partial chain to a block, it gets put here.
+
 	Indirect *partial;
 	int left;
+
+// How deep does the block reside?
+
 	int depth = block_to_path(inode, block, offsets);
+
+// A valid block must have a depth of at least one. If it doesn't, something is
+// very wrong.
 
 	if (depth == 0)
 		goto out;
 
+// If goto is considered harmful, pregnant women may wish to stay away from this
+// function. Execution may be sent back to this point if we need to try reading
+// for the block again.
+
 reread:
+
+// Try and get the path to the block.
+
 	partial = get_branch(inode, depth, offsets, chain, &err);
+
+// This `if` statement can be entered either because the `get_branch` call was
+// successful, or from a goto further down. In either case, getting in here
+// means the block was found one way or another.
 
 	/* Simplest case - block found, no allocation needed */
 	if (!partial) {
 got_it:
+
+// Set the device, block number, and block size of the buffer.
+
 		map_bh(bh, inode->i_sb, block_to_cpu(chain[depth-1].key));
+
+// Make `partial` refer to the whole block chain and jump to cleanup.
+
 		/* Clean up and exit */
 		partial = chain+depth-1; /* the whole chain */
 		goto cleanup;
 	}
 
+// Another confusing `if` statement. It can either be reached because of an
+// error with looking up or reading a block, _or_ after a successful block read
+// where normal cleanup needs to happen.
+
 	/* Next simple case - plain lookup or failed read of indirect block */
+
+// Getting in because of an error.
+
 	if (!create || err == -EIO) {
+
+// Getting in as part of normal cleanup.
+
 cleanup:
+
+// Free the buffers in the block chain.
+
 		while (partial > chain) {
 			brelse(partial->bh);
 			partial--;
 		}
+
+// And return, whether this was reached because of an error calculating the path
+// to the desired block or as part of the normal program execution.
+
 out:
 		return err;
 	}
@@ -439,8 +489,14 @@ out:
 	 * reading it. Handling of that case (forget what we've got and
 	 * reread) is taken out of the main path.
 	 */
+
+// Trying to read the block again, after jumping down to `changed` to free the
+// buffers in the partial chain.
+
 	if (err == -EAGAIN)
 		goto changed;
+
+// Allocate a new branch to the block and splice the chain together.
 
 	left = (chain + depth) - partial;
 	err = alloc_branch(inode, left, offsets+(partial-chain), partial);
@@ -450,16 +506,29 @@ out:
 	if (splice_branch(inode, chain, partial, left) < 0)
 		goto changed;
 
+// Set the buffer as new before jumping to `got_it` and continuing on as if the
+// block had been found normally and setting those settings on the buffer
+// mentioned up there.
+
 	set_buffer_new(bh);
 	goto got_it;
 
 changed:
+
+// Free the buffers in the block chain before trying to reread the block again.
+
 	while (partial > chain) {
 		brelse(partial->bh);
 		partial--;
 	}
+
+// Jump back up to reread to try again.
+
 	goto reread;
 }
+
+// Is this range of block pointers all zeroed out? Returns false if any block
+// pointer between `p` and `q` is not.
 
 static inline int all_zeroes(block_t *p, block_t *q)
 {
@@ -468,6 +537,9 @@ static inline int all_zeroes(block_t *p, block_t *q)
 			return 0;
 	return 1;
 }
+
+// Find the shared branches in the block chains this inode is using. `depth`,
+// not too surprisingly, tells us how deep we'll need to go.
 
 static Indirect *find_shared(struct inode *inode,
 				int depth,
@@ -479,42 +551,90 @@ static Indirect *find_shared(struct inode *inode,
 	int k, err;
 
 	*top = 0;
+
+// `k` is the new `depth`. Decrement `k` while it's greater than 1 and the
+// contents of `offsets` at that level are empty. Once that's done, that's the
+// new depth for the blocks that need to be found.
+
 	for (k = depth; k > 1 && !offsets[k-1]; k--)
 		;
+
+// Get the block chain at this depth.
+
 	partial = get_branch(inode, k, offsets, chain, &err);
 
+// Take a write mutex.
+
 	write_lock(&pointers_lock);
+
+// If `partial` is NULL, make `partial` be the full chain offset by the depth
+// (minus 1, of course). So, for example, if `partial` is NULL and we're finding
+// the shared branch at a depth of 2, `partial` is set to the full chain
+// starting at the second element (1).
+
 	if (!partial)
 		partial = chain + k-1;
 	if (!partial->key && *partial->p) {
+
+// Release the write mutex and goto `no_top` so whatever's in `partial` at this
+// point can be returned.
+
 		write_unlock(&pointers_lock);
 		goto no_top;
 	}
+
+// Move `p` back past any blocks that are all zeroed out.
+
 	for (p=partial;p>chain && all_zeroes((block_t*)p->bh->b_data,p->p);p--)
 		;
+
+// If `p` didn't change at all and is not identical to the block chain pointer,
+// decrement the `p->p` block pointer once.
+
 	if (p == chain + k - 1 && p > chain) {
 		p->p--;
 	} else {
+
+// Otherwise, set the `*top` block pointer to `p`'s block pointer and set `p`'s
+// block pointer to 0. Those blocks will be freed separately from the ones in
+// `partial`.
+
 		*top = *p->p;
 		*p->p = 0;
 	}
+
+// Release the mutex.
+
 	write_unlock(&pointers_lock);
+
+// Free the buffers that `p` skipped over because their blocks were zeroed out.
 
 	while(partial > p)
 	{
 		brelse(partial->bh);
 		partial--;
 	}
+
+// Return the shared branch.
+
 no_top:
 	return partial;
 }
+
+// Frees the blocks on this inode between block pointers `*p` and `*q`.
 
 static inline void free_data(struct inode *inode, block_t *p, block_t *q)
 {
 	unsigned long nr;
 
+// Loop over the blocks from `p` to `q`, not going past `q`.
+
 	for ( ; p < q ; p++) {
 		nr = block_to_cpu(*p);
+
+// If this block pointer is actually pointing at anything, set said pointer to
+// zero and free that block.
+
 		if (nr) {
 			*p = 0;
 			xiafs_free_block(inode, nr);
@@ -522,29 +642,57 @@ static inline void free_data(struct inode *inode, block_t *p, block_t *q)
 	}
 }
 
+// Free whole branches of blocks. This function recursively calls itself at each
+// depth level, freeing the intermediate blocks with indirect block pointers,
+// until it reaches the end of each branch, whereupon it calls `free_data` to
+// actually free the data blocks.
+
 static void free_branches(struct inode *inode, block_t *p, block_t *q, int depth)
 {
 	struct buffer_head * bh;
 	unsigned long nr;
 
+// If it's not at the end of the branch, decrement the depth by one.
+
 	if (depth--) {
+
+// Loop through the block pointers on this level.
+
 		for ( ; p < q ; p++) {
+
+// Get the block pointer. If the pointer is NULL, continue on.
+
 			nr = block_to_cpu(*p);
 			if (!nr)
 				continue;
 			*p = 0;
+
+// Read the block. If there's no buffer returned, continue on.
+
 			bh = sb_bread(inode->i_sb, nr);
 			if (!bh)
 				continue;
+
+// Call `free_branches` on the block pointers in this block and release the
+// buffer.
+
 			free_branches(inode, (block_t*)bh->b_data,
 				      block_end(bh), depth);
 			bforget(bh);
+
+// Free this block and mark the inode as dirty.
+
 			xiafs_free_block(inode, nr);
 			mark_inode_dirty(inode);
 		}
+
+// Otherwise just free this blocks. No need to look further.
+
 	} else
 		free_data(inode, p, q);
 }
+
+// Truncate the file, nuking all the blokcs currently assigned to the inode.
 
 static inline void truncate (struct inode * inode)
 {
@@ -558,12 +706,27 @@ static inline void truncate (struct inode * inode)
 	int first_whole;
 	long iblock;
 
+// Get the last block the inode is using.
+
 	iblock = (inode->i_size + sb->s_blocksize -1) >> sb->s_blocksize_bits;
+
+// This is off in `buffer.c` in the kernel. That `get_block` in the arguments is
+// in fact our very own `get_block` function in this file. This zeroes out the
+// pages of memory the inode has.
+
 	block_truncate_page(inode->i_mapping, inode->i_size, get_block);
 
+// How deep is the last block?
+
 	n = block_to_path(inode, iblock, offsets);
+
+// If there are no blocks, there's nothing to truncate.
+
 	if (!n)
 		return;
+
+// Nothing but direct blocks to free. Free those, then skip down to
+// `do_indirects` and clear those blocks if there's anything there.
 
 	if (n == 1) {
 		free_data(inode, idata+offsets[0], idata + DIRECT);
@@ -571,27 +734,68 @@ static inline void truncate (struct inode * inode)
 		goto do_indirects;
 	}
 
+// If this inode is only using direct blocks, `first_whole` is set to 0 above.
+// However, if there are indirect blocks of any sort `first_whole` is set to
+// the level of indirection used. If this inode is only using singly indirect
+// block pointers, first_whole is set to 1. If it's using doubly indirect block
+// pointers, it's set to 2, which means that `while` loop at `do_indirects`
+// doesn't end up being called at all. Basically, `first_whole` is to indicate
+// which of the indirect block pointers don't get cleared after this.
+
 	first_whole = offsets[0] + 1 - DIRECT;
+
+// Find the shared branches this inode is using for its block chains.
+
 	partial = find_shared(inode, n, offsets, chain, &nr);
+
+// If `nr` is set, there's some extra blocks to free outside of the partial
+// chain.
+
 	if (nr) {
+
+// Mark either the inode or just that buffer as dirty, depending on whether
+// those block chains are the same, and free the blocks `nr` is pointing to. If
+// `partial` and `chain` are pointing to the same place, these are the only
+// blocks to free.
+
 		if (partial == chain)
 			mark_inode_dirty(inode);
 		else
 			mark_buffer_dirty_inode(partial->bh, inode);
 		free_branches(inode, &nr, &nr+1, (chain+n-1) - partial);
 	}
+
+// Now clear the blocks in `partial` out, unless it's the same as the chain.
+
 	/* Clear the ends of indirect blocks on the shared branch */
 	while (partial > chain) {
+
+// Free the blocks in this branch, mark the buffer as dirty and release the
+// buffer.
 		free_branches(inode, partial->p + 1, block_end(partial->bh),
 				(chain+n-1) - partial);
 		mark_buffer_dirty_inode(partial->bh, inode);
 		brelse (partial->bh);
+
+// Decrement the `partial` block pointer and do it agin to the next level of the
+// chain.
+
 		partial--;
 	}
+
+// If there are any indirect block pointers remaining to be cleared, do so now.
+
 do_indirects:
 	/* Kill the remaining (whole) subtrees */
 	while (first_whole < DEPTH-1) {
+
+// Is this block pointer even set?
+
 		nr = idata[DIRECT+first_whole];
+
+// If it is, set the pointer to NULL, mark the inode as dirty and free any
+// branches of the block chain associated with this block pointer.
+
 		if (nr) {
 			idata[DIRECT+first_whole] = 0;
 			mark_inode_dirty(inode);
@@ -599,9 +803,17 @@ do_indirects:
 		}
 		first_whole++;
 	}
+
+// Set `mtime` and `ctime` to now.
+
 	inode_set_mtime_to_ts(inode, inode_set_ctime_current(inode));
+
+// Mark the inode as so, so dirty.
+
 	mark_inode_dirty(inode);
 }
+
+// Count the number of blocks that would be used for a given size.
 
 static inline unsigned nblocks(loff_t size, struct super_block *sb)
 {
@@ -621,11 +833,16 @@ static inline unsigned nblocks(loff_t size, struct super_block *sb)
 
 /* end former itree_common.c */
 
+// Just a wrapper around `get_block` to use outside of this file to better fit
+// with existing code.
+
 int xiafs_get_block(struct inode * inode, sector_t block,
 			struct buffer_head *bh_result, int create)
 {
 	return get_block(inode, block, bh_result, create);
 }
+
+// Truncate the inode, unless it's not a regular file.
 
 void xiafs_truncate(struct inode * inode)
 {
@@ -633,6 +850,8 @@ void xiafs_truncate(struct inode * inode)
 		return;
 	truncate(inode);
 }
+
+// Return the number of blocks for the given size.
 
 unsigned xiafs_blocks(loff_t size, struct super_block *sb)
 {
